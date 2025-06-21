fb245b7bc6d7c44199e14017b556d051266cc914
ushiyushi <ymurae46@gmail.com>
2025-06-21 10:06:29 +0900

    ユーザーモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :name_sei, presence: true
+  validates :name_mei, presence: true
+  validates :kana_sei, presence: true
+  validates :kana_mei, presence: true
+  validates :birthday, presence: true
+
+  has_many :items
+  has_many :purchases
 end
