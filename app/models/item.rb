af923be52e946adc24c8cba957e67711b2c0010c
ushiyushi <ymurae46@gmail.com>
2025-06-24 10:32:39 +0900

    エラーメッセージの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,13 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
   validates :item_name, presence: true
   validates :introduction, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 }
-  validates :condition_id, presence: true, numericality: { other_than: 1 }
-  validates :fee_id, presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 }
-  validates :delivery_time_id, presence: true, numericality: { other_than: 1 }
-  validates :price, presence: true, numericality: {only_integer: true, in: 300..9_999_999},
-             format: {with: /\A[0-9]+\z/, message: "must be half‑width numeric"}
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :fee_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: {only_integer: true, in: 300..9_999_999}
 
   belongs_to :user
   has_one_attached :image
