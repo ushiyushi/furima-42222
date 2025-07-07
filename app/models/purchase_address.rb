18abc04f2076c7e3f4787a3fa3db3fdbea62d9d0
ushiyushi <ymurae46@gmail.com>
2025-07-07 15:59:34 +0900

    フォームオブジェクトの作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,14 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user, :item, :post_code, :prefecture_id, :city, :street_address, :building, :phone, :purchase, :user_id
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :user_id
+  end
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    
+  end
+end
